6465341ee5d706b1f6d0f18a11bbbfe2ccadc537
jlflaminia <jlflaminia1@gmail.com>
2025-05-16 08:38:33 +0800

    sql

--- masterdiy (2).sql
+++ masterdiy (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2025 at 06:53 AM
+-- Generation Time: May 16, 2025 at 02:37 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -45,7 +45,19 @@ INSERT INTO `comments` (`id`, `guide_id`, `username`, `comment`, `created_at`) V
 (3, 4, 'siomai', 'sdsdfhsdrh', '2025-05-14 04:22:24'),
 (4, 4, 'siomai', 'ampanget hahhaha', '2025-05-14 04:23:04'),
 (7, 4, 'siomai', 'sfthsrths', '2025-05-14 04:31:17'),
-(8, 4, 'siomai', 'srhdrth', '2025-05-14 04:36:03');
+(8, 4, 'siomai', 'srhdrth', '2025-05-14 04:36:03'),
+(0, 2, 'jl', 'hahaha panget!', '2025-05-15 04:57:12'),
+(0, 2, 'jl', 'hahah gago', '2025-05-15 04:59:13'),
+(0, 2, 'jl', 'gago', '2025-05-15 05:00:10'),
+(0, 0, 'simon', '123123123', '2025-05-15 06:41:19'),
+(0, 0, 'simon', 'w5tw45t', '2025-05-15 06:42:38'),
+(0, 0, 'jl', 'sgsrtg', '2025-05-15 06:43:02'),
+(0, 0, 'jl', 'siomai rice pabile!', '2025-05-15 06:43:48'),
+(0, 0, 'bona', 'ilan po', '2025-05-15 06:44:26'),
+(0, 0, 'jl', 'dfhrth', '2025-05-15 06:47:56'),
+(0, 0, 'bona', '12123', '2025-05-15 06:48:45'),
+(0, 0, 'jl', 'what?', '2025-05-16 00:29:50'),
+(0, 0, 'simeon', 'wat dapak!', '2025-05-16 00:30:42');
 
 -- --------------------------------------------------------
 
@@ -76,9 +88,7 @@ CREATE TABLE `guides` (
 
 INSERT INTO `guides` (`id`, `guide_type`, `device`, `part`, `title`, `introduction`, `difficulty_estimate`, `tools`, `conclusion`, `steps`, `step_type`, `wisdom`, `created_at`, `step_images`) VALUES
 (1, 'Replacement', 'iPhone 16', 'Battery', 'iphone battery replacement', 'rgsergsergerger', 'Moderate', 'jlafarjfer3', 'sdfgsdgsdrg', '[\"step 1\",\"step 2\"]', '[]', '[\"ardgaerg\",\"aergsergserg\"]', '2025-05-13 13:05:25', NULL),
-(2, 'Replacement', 'iPhone 16', 'Battery', 'battery replacement', 'aegaergerg', 'Moderate', 'aergegergae', 'rgaergrjhaegvgrva', '[\"step 1\",\"step 2\",\"step 3\"]', '[]', '[\"sdtsrthrsthsth\",\"athrthaerfdfvasdv\",\"rsthsrtsrtbsr\"]', '2025-05-13 13:28:25', '[\"uploads\\/stepimg_682348f902179_494359325_1053978556580148_2521357988331885254_n.png\",\"uploads\\/stepimg_682348f90ca13_494358854_982913163827000_4533838095770380835_n.png\",\"uploads\\/stepimg_682348f90d1dc_494357658_1680618539214175_1124004200284624763_n.png\"]'),
-(3, 'replacement', 'ip17', 'batt', 'iphone battery ni simoen', 'swrthsrth', 'Moderate', 'asrthsrth', 'asrthsarehshrt', '[\"step 1\",\"step 2\",\"step 3\"]', '[]', '[\"srtjhrsdth\",\"dtyjktedyjdtyj\",\"rjhrsjh\"]', '2025-05-14 01:33:32', '[\"uploads\\/stepimg_6823f2ec082ed_repair2.jpg\",\"uploads\\/stepimg_6823f2ec08377_repair.jpg\",\"uploads\\/stepimg_6823f2ec083d3_ip-logo.png\"]'),
-(4, 'sdthst', 'hsrthsrt', 'hrsthsr', 'hsrth', 'rdthserth', 'Easy', 'rasehsr', 'srthsrth', '[\"\"]', '[]', '[\"saerhsrthsrth\"]', '2025-05-14 03:05:55', '[\"uploads\\/stepimg_68240893d06a4_repair2.jpg\"]');
+(2, 'Replacement', 'iPhone 16', 'Battery', 'battery replacement', 'aegaergerg', 'Moderate', 'aergegergae', 'rgaergrjhaegvgrva', '[\"step 1\",\"step 2\",\"step 3\"]', '[]', '[\"sdtsrthrsthsth\",\"athrthaerfdfvasdv\",\"rsthsrtsrtbsr\"]', '2025-05-13 13:28:25', '[\"uploads\\/stepimg_682348f902179_494359325_1053978556580148_2521357988331885254_n.png\",\"uploads\\/stepimg_682348f90ca13_494358854_982913163827000_4533838095770380835_n.png\",\"uploads\\/stepimg_682348f90d1dc_494357658_1680618539214175_1124004200284624763_n.png\"]');
 
 -- --------------------------------------------------------
 
@@ -105,18 +115,12 @@ INSERT INTO `users` (`id`, `username`, `password`, `usertype`) VALUES
 (32, 'lourdes', '$2y$10$AWm.5x0v.aoammPwLAhPnO2a1lkbGB4dMmw6jQC0tLW7uh4ZHVQAq', 'user'),
 (33, 'admin', '$2y$10$a8gHtP4drlqBPOePHRDc1.bjJe7P.E7jUi22am2XBZOS.DUXEg4qq', 'admin'),
 (34, 'jl', '$2y$10$hna8W98MjpNx.TbLB2oSgOIOBj6p5ArBQJUUpi9xSu.reLTqqpBDK', 'user'),
-(35, 'siomai', '$2y$10$/Rtfi8q1gSsT70zhQnxgpe8AyQQ39eC3DSfo89C0JVM7RZyLOxJUK', 'user');
+(35, 'simon', '$2y$10$k2x4.vjN1vM33Yn2rsu0JeyBzDtGkDWzEj6xZAZBdvCjNFwUhHICW', 'user'),
+(36, 'bona', '$2y$10$7hSqwsALeatC8jg0a4q3v.7O.Ia7PAG1YWpAG/CscteRxNGEffLBu', 'user');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `guide_id` (`guide_id`);
 
 --
 -- Indexes for table `guides`
@@ -136,32 +140,16 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
 -- AUTO_INCREMENT for table `guides`
 --
 ALTER TABLE `guides`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`guide_id`) REFERENCES `guides` (`id`) ON DELETE CASCADE;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
